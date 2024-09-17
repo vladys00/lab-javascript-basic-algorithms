8ca0b1564c0d6f7c20fbd6bb81477ae62e58ba3b
Vladys K <vldk0022@gmail.com>
2024-09-17 15:09:38 +0200

    Three main iterations done

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 // Iteration 1: Names and Input
+let hacker1 = "Julio";
 
+console.log(`The driver's name is ${hacker1}`)
+
+let hacker2 = "Roberto";
+
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length){
+   console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`) ;
+} else {
+    console.log("Wow, you both have equally long names, XX characters!");
+}
 
 // Iteration 3: Loops
+let stored = " ";
+
+for (let i = 0; i < hacker1.length; i++){
+    
+    stored += hacker1[i] + " ";
+    
+}
+console.log(stored.toUpperCase());
+
+let reverseStored = " ";
+
+for (let j = hacker1.length - 1; j >= 0 ; j--){
+    
+    reverseStored += hacker1[j];
+    
+}
+console.log(reverseStored);
